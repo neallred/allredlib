bfd396138a5994efdcbd03512079dad610fec5be
neallred <neallred@gmail.com>
2020-05-16 02:52:36 -0500

    Stub out new UI

--- client/src/Main.purs
+++ client/src/Main.purs
@@ -1,13 +1,14 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, bind, unit)
+
+import Button
 import Effect (Effect)
-import List as List
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
-import Effect.Console (log)
+
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI List.component unit body
+  runUI component unit body

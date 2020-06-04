95eca9a159431e7ea1fb08ce628c0423584d8425
neallred <neallred@gmail.com>
2020-06-03 20:35:11 -0500

    Load seed data into seeder

--- src/Seed.hs
+++ src/Seed.hs
@@ -6,6 +6,7 @@ module Seed where
 
 import GHC.Generics
 import Data.Aeson
+import Data.Either (fromRight)
 import qualified Data.ByteString.Lazy as B
 
 import Import
@@ -14,21 +15,21 @@ import BookCondition
 import Language
 import System.Environment (getEnvironment)
 
-data ImportedTitle = ImportedTitle
-  String -- title id
-  String -- title
+data SeedTitle = SeedTitle
+  Text -- title id
+  Text -- title
   (Maybe Int) -- year
-  (Maybe String) -- synopsis
-  (Maybe String) -- seriesPart
-  (Maybe String) -- seriesId
-  (Maybe String) -- subseriesPart
-  (Maybe String) -- subseriesId
+  (Maybe Text) -- synopsis
+  (Maybe Text) -- seriesPart
+  (Maybe Text) -- seriesId
+  (Maybe Text) -- subseriesPart
+  (Maybe Text) -- subseriesId
   deriving (Generic, Show)
 
-instance ToJSON ImportedTitle where
-  toJSON (ImportedTitle id title year synopsis seriesPart seriesId subseriesPart subseriesId) =
+instance ToJSON SeedTitle where
+  toJSON (SeedTitle seedId title year synopsis seriesPart seriesId subseriesPart subseriesId) =
     object
-    [ "id" .= id
+    [ "id" .= seedId
     , "title" .= title
     , "year" .= year
     , "synopsis" .= synopsis
@@ -38,9 +39,9 @@ instance ToJSON ImportedTitle where
     , "subseriesId" .= subseriesId
     ]
 
-instance FromJSON ImportedTitle where
+instance FromJSON SeedTitle where
   parseJSON (Object v) =
-    ImportedTitle <$> v .: "id"
+    SeedTitle <$> v .: "id"
                   <*> v .: "title"
                   <*> v .: "year"
                   <*> v .: "synopsis"
@@ -50,18 +51,18 @@ instance FromJSON ImportedTitle where
                   <*> v .: "subseriesId"
   parseJSON _ = mzero
 
-data ImportedCreator = ImportedCreator
-  String -- id
-  (Maybe String) -- bio
+data SeedCreator = SeedCreator
+  Text -- id
+  (Maybe Text) -- bio
   (Maybe Int) -- birth
   (Maybe Int) -- death
-  String -- firstName
-  (Maybe String) -- lastName
+  Text -- firstName
+  (Maybe Text) -- lastName
   deriving (Generic, Show)
 
-instance FromJSON ImportedCreator where
+instance FromJSON SeedCreator where
   parseJSON (Object v) =
-    ImportedCreator <$> v .: "id"
+    SeedCreator <$> v .: "id"
                     <*> v .: "bio"
                     <*> v .: "birth"
                     <*> v .: "death"
@@ -69,10 +70,10 @@ instance FromJSON ImportedCreator where
                     <*> v .: "lastName"
   parseJSON _ = mzero
 
-instance ToJSON ImportedCreator where
-  toJSON (ImportedCreator id bio birth death firstName lastName) =
+instance ToJSON SeedCreator where
+  toJSON (SeedCreator seedId bio birth death firstName lastName) =
     object
-    [ "id" .= id
+    [ "id" .= seedId
     , "bio" .= bio
     , "birth" .= birth
     , "death" .= death
@@ -80,15 +81,150 @@ instance ToJSON ImportedCreator where
     , "lastName" .= lastName
     ]
 
+data SeedCreatorTitle = SeedCreatorTitle
+  Text -- creatorId
+  Text -- titleId
+  deriving (Generic, Show)
+
+instance FromJSON SeedCreatorTitle where
+  parseJSON (Object v) =
+    SeedCreatorTitle <$> v .: "creatorId"
+                    <*> v .: "titleId"
+  parseJSON _ = mzero
+
+instance ToJSON SeedCreatorTitle where
+  toJSON (SeedCreatorTitle creatorId titleId) =
+    object
+    [ "creatorId" .= creatorId
+    , "titleId" .= titleId
+    ]
+
+data SeedGenreTitle = SeedGenreTitle
+  Text -- genreId
+  Text -- titleId
+  deriving (Generic, Show)
+
+instance FromJSON SeedGenreTitle where
+  parseJSON (Object v) =
+    SeedGenreTitle <$> v .: "genreId"
+                   <*> v .: "titleId"
+  parseJSON _ = mzero
+
+instance ToJSON SeedGenreTitle where
+  toJSON (SeedGenreTitle genreId titleId) =
+    object
+    [ "genreId" .= genreId
+    , "titleId" .= titleId
+    ]
+
+data SeedSeries = SeedSeries
+  Text -- id
+  (Maybe Text) -- synopsis
+  Text -- title
+  Int -- totalBookMembers
+  Int -- totalSubseries
+  [Text] -- attribution ids
+  deriving (Generic, Show)
+
+instance FromJSON SeedSeries where
+  parseJSON (Object v) =
+    SeedSeries <$> v .: "id"
+                   <*> v .: "synopsis"
+                   <*> v .: "title"
+                   <*> v .: "totalBookMembers"
+                   <*> v .: "totalSubseries"
+                   <*> v .: "attributionIds"
+  parseJSON _ = mzero
+
+instance ToJSON SeedSeries where
+  toJSON (SeedSeries seedId synopsis title totalBookMembers totalSubseries attributionIds) =
+    object
+    [ "id" .= seedId
+    , "synopsis" .= synopsis
+    , "title" .= title
+    , "totalBookMembers" .= totalBookMembers
+    , "totalSubseries" .= totalSubseries
+    , "attributionIds" .= attributionIds
+    ]
+
+data SeedSubseries = SeedSubseries
+  Text -- id
+  Text -- seriesId
+  (Maybe Text) -- synopsis
+  Text -- title
+  Int -- totalBookMembers
+  [Text] -- attribution ids
+  deriving (Generic, Show)
+
+instance FromJSON SeedSubseries where
+  parseJSON (Object v) =
+    SeedSubseries <$> v .: "id"
+                   <*> v .: "seriesId"
+                   <*> v .: "synopsis"
+                   <*> v .: "title"
+                   <*> v .: "totalBookMembers"
+                   <*> v .: "attributionIds"
+  parseJSON _ = mzero
+
+instance ToJSON SeedSubseries where
+  toJSON (SeedSubseries seedId seriesId synopsis title totalBookMembers attributionIds) =
+    object
+    [ "id" .= seedId
+    , "seriesId" .= seriesId
+    , "synopsis" .= synopsis
+    , "title" .= title
+    , "totalBookMembers" .= totalBookMembers
+    , "attributionIds" .= attributionIds
+    ]
+
+data SeedAttribution = SeedAttribution
+  Text -- id
+  Text -- title
+  (Maybe Text) -- link
+  (Maybe UTCTime) -- accessed
+  (Maybe UTCTime) -- yearCreated
+  (Maybe Text) -- placePublished
+  (Maybe Text) -- author
+  (Maybe Text) -- publisher
+  deriving (Generic, Show)
+
+instance FromJSON SeedAttribution where
+  parseJSON (Object v) =
+    SeedAttribution <$> v .: "id"
+                    <*> v .: "title"
+                    <*> v .: "link"
+                    <*> v .: "accessed"
+                    <*> v .: "yearCreated"
+                    <*> v .: "placePublished"
+                    <*> v .: "author"
+                    <*> v .: "publisher"
+  parseJSON _ = mzero
+
+instance ToJSON SeedAttribution where
+  toJSON (SeedAttribution seedId title seedLink accessed yearCreated placePublished author publisher) =
+    object
+    [ "id" .= seedId
+    , "title" .= title
+    , "link" .= seedLink
+    , "accessed" .= accessed
+    , "yearCreated" .= yearCreated
+    , "placePublished" .= placePublished
+    , "author" .= author
+    , "publisher" .= publisher
+    ]
+
 findWithDefault :: (a -> Bool) -> a -> [a] -> a
-findWithDefault f z [] = z
+findWithDefault _ z [] = z
 findWithDefault f z (x:xs) =
   if f x then x else Seed.findWithDefault f z xs
+
+countList :: [a] -> Text
+countList xs = (pack . show . length) xs
 
 runImporter :: IO ()
 runImporter = do
   env <- getEnvironment
-  let (_, seed_path) = Seed.findWithDefault (\(k, v) -> k == "ALLREDLIB_SEED_PATH") ("", "") env
+  let (_, seed_path) = Seed.findWithDefault (\(k, _) -> k == "ALLREDLIB_SEED_PATH") ("", "") env
   titlesStr <- B.readFile $ seed_path ++ "/Title.json"
   creatorsStr <- B.readFile $ seed_path ++ "/Creator.json"
   creatorTitlesStr <- B.readFile $ seed_path ++ "/CreatorTitle.json"
@@ -97,17 +233,21 @@ runImporter = do
   subseriesStr <- B.readFile $ seed_path ++ "/Subseries.json"
   -- genreTitles genre ids match the ordering of enums, indexed starting with 1
   genreTitlesStr <- B.readFile $ seed_path ++ "/GenreTitle.json"
-  let eitherTitles = (eitherDecode titlesStr) :: Either String [ImportedTitle]
-  let numTitles = case eitherTitles of
-                    Left _ -> 0
-                    Right titles -> length titles
-  let eitherCreators = (eitherDecode creatorsStr) :: Either String [ImportedCreator]
-  let numCreators = case eitherCreators of
-                    Left _ -> 0
-                    Right creators -> length creators
+  let eitherTitles = (eitherDecode titlesStr) :: Either String [SeedTitle]
+  let eitherCreators = (eitherDecode creatorsStr) :: Either String [SeedCreator]
+  let eitherCreatorTitles = (eitherDecode creatorTitlesStr) :: Either String [SeedCreatorTitle]
+  let eitherAttributions = (eitherDecode attributionsStr) :: Either String [SeedAttribution]
+  let eitherSeries = (eitherDecode seriesStr) :: Either String [SeedSeries]
+  let eitherSubseries = (eitherDecode subseriesStr) :: Either String [SeedSubseries]
+  let eitherGenreTitles = (eitherDecode genreTitlesStr) :: Either String [SeedGenreTitle]
   putStrLn "Need to seed:"
-  putStrLn $ (pack . show $ numTitles) ++ " titles"
-  putStrLn $ (pack . show $ numCreators) ++ " creators"
+  putStrLn $ (countList $ fromRight [] eitherTitles) ++ " titles"
+  putStrLn $ (countList $ fromRight [] eitherCreators) ++ " creators"
+  putStrLn $ (countList $ fromRight [] eitherCreatorTitles) ++ " creatorTitles"
+  putStrLn $ (countList $ fromRight [] eitherAttributions) ++ " attributions"
+  putStrLn $ (countList $ fromRight [] eitherSeries) ++ " series"
+  putStrLn $ (countList $ fromRight [] eitherSubseries) ++ " subseries"
+  putStrLn $ (countList $ fromRight [] eitherGenreTitles) ++ " genreTitles"
   pure ()
 
 lotrSeries :: Import.Series
@@ -158,8 +298,8 @@ makeManifestationPrinting :: Key Manifestation -> Key Printing -> ManifestationP
 makeManifestationPrinting mId pId = ManifestationPrinting mId pId Nothing Nothing
 
 makeLotrEdition :: ImprintPublisherId -> TitleId -> Edition
-makeLotrEdition imprintPublisherId titleId = 
-  Edition 
+makeLotrEdition imprintPublisherId titleId =
+  Edition
     (Just BookBinding.PerfectBound)
     (Just Language.English)
     "Cheap paperback edition"
@@ -170,7 +310,7 @@ makeLotrEdition imprintPublisherId titleId =
     Nothing
 
 makeLotrPrinting :: Key Edition -> Text -> Int -> Int -> Printing
-makeLotrPrinting editionId isbn year pages = 
+makeLotrPrinting editionId isbn year pages =
   Printing
     editionId
     (Just isbn)
@@ -190,7 +330,7 @@ torImprint =
 
 jrrTolkien :: Creator
 jrrTolkien =
-  Creator 
+  Creator
   (Just "J.R.R. Tolkien grew up in South Africa. He was an air warden in W.W.I.I.")
   (Just 1892)
   (Just 1973)
@@ -199,4 +339,4 @@ jrrTolkien =
 
 makeImprintPublisher :: ImprintId -> PublisherId -> ImprintPublisher
 makeImprintPublisher =
-  ImprintPublisher 
+  ImprintPublisher
